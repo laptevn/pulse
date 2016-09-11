6b9c9091412445ec439dfab9f36d9156952e2405
Nickolay Laptev <laptev@transct.ru>
2016-09-11 08:13:31 +0300

    Added calculation of all given answers

--- src/main/java/com/pulse/poll/PollCalculator.java
+++ src/main/java/com/pulse/poll/PollCalculator.java
@@ -2,14 +2,15 @@ package com.pulse.poll;
 
 import com.pulse.QuestionContext;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PollCalculator {
     private final QuestionContext questionContext;
     private final int numberOfRegisteredUsers;
     private int numberOfUsersWithAnswers;
     private final Set<String> usersWithCorrectAnswers = new HashSet<>();
+    private final Map<Integer, Integer> answerToCount = new HashMap<>();
 
     public PollCalculator(QuestionContext questionContext, int numberOfRegisteredUsers) {
         this.questionContext = questionContext;
@@ -21,6 +22,11 @@ public class PollCalculator {
         if (answer == questionContext.getCorrectAnswerIndex() + 1) {
             usersWithCorrectAnswers.add(userId);
         }
+
+        if (!answerToCount.containsKey(answer)) {
+            answerToCount.put(answer, 0);
+        }
+        answerToCount.put(answer, answerToCount.get(answer) + 1);
     }
 
     public boolean allUsersGaveAnswers() {
@@ -29,14 +35,47 @@ public class PollCalculator {
 
     public String calculateGeneralResult() {
         int percent = numberOfUsersWithAnswers == 0 ? 0 : usersWithCorrectAnswers.size() * 100 / numberOfUsersWithAnswers;
-        return String.format(
-                "Опрос окончен. Всего %d человек из %d поучаствовали в опросе.%nБыло дано %d%% правильных ответов.",
-                numberOfUsersWithAnswers,
-                numberOfRegisteredUsers,
-                percent);
+
+        StringBuilder result = new StringBuilder();
+        result.append("Опрос окончен. Всего ")
+                .append(numberOfUsersWithAnswers)
+                .append(" человек из ")
+                .append(numberOfRegisteredUsers)
+                .append(" поучаствовали в опросе.")
+                .append(System.lineSeparator());
+        result.append("Было дано ").append(percent).append("% правильных ответов.");
+
+        for (AnswerCountPair pair : getAnswersSortedByCount()) {
+            result.append(System.lineSeparator());
+            result.append(pair.count).append(" человек выбрали вариант №").append(pair.answer);
+        }
+
+        return result.toString();
+    }
+
+    private List<AnswerCountPair> getAnswersSortedByCount() {
+        return answerToCount.entrySet().stream()
+                .map(entry -> new AnswerCountPair(entry.getKey(), entry.getValue()))
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public String calculatePersonalResult(String userId) {
         return usersWithCorrectAnswers.contains(userId) ? "Поздравляем, вы дали правильный ответ." : "Вы не дали правильный ответ.";
+    }
+
+    private static class AnswerCountPair implements Comparable<AnswerCountPair> {
+        public final Integer answer;
+        public final Integer count;
+
+        public AnswerCountPair(Integer answer, Integer count) {
+            this.answer = answer;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(AnswerCountPair o) {
+            return -count.compareTo(o.count);
+        }
     }
 }
