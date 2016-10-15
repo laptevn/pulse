66f757affad3947d9c5002bf0810312c9142dc10
Nickolay Laptev <laptev@transct.ru>
2016-10-16 00:11:32 +0300

    Added a logging of incoming messages. Added an integration with distributed logging Graylog.

--- src/main/java/com/pulse/PulseBot.java
+++ src/main/java/com/pulse/PulseBot.java
@@ -3,6 +3,8 @@ package com.pulse;
 import com.pulse.datasource.DataSource;
 import com.pulse.poll.PollSession;
 import com.pulse.request.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.api.objects.Message;
@@ -15,6 +17,8 @@ import java.util.Set;
 
 @Component
 public class PulseBot extends TelegramLongPollingBot {
+    private static final Logger LOGGER = LogManager.getLogger(PulseBot.class);
+
     private final RequestHandler requestHandler;
     private final BotProperties botProperties;
     private final PollSession pollSession;
@@ -33,11 +37,25 @@ public class PulseBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         if(update.hasMessage()){
             Message message = update.getMessage();
+            LOGGER.info("Received a message {}", () -> messageToString(message));
 
             if (!requestHandler.handle(message, new RequestContext(messageSender, registeredUsers, pollSession))) {
+                LOGGER.warn("Cannot handle the message");
                 messageSender.send(message.getChatId().toString(), "Не могу обработать ваш запрос.");
             }
         }
+    }
+
+    private static String messageToString(Message message) {
+        StringBuilder result = new StringBuilder();
+        result.append("From: ").append(message.getFrom().toString()).append(". ");
+        if (message.hasText()) {
+            result.append("Text: '").append(message.getText()).append("'");
+        } else {
+            result.append("No text");
+        }
+
+        return result.toString();
     }
 
     @Override
